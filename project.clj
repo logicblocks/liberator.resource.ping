ef8c5c6c90ab0512335064fa7caeafb959f5217c
Toby Clemson <tobyclemson@gmail.com>
2020-03-22 13:53:03 +0000

    Add leiningen plugins, precommit command and release configuration.

--- project.clj
+++ project.clj
@@ -7,15 +7,63 @@
 
   :dependencies [[b-social/liberator-mixin "0.0.48"]]
 
-  :plugins [[lein-eftest "0.5.8"]]
+  :plugins [[lein-cloverage "1.1.1"]
+            [lein-shell "0.5.0"]
+            [lein-ancient "0.6.15"]
+            [lein-changelog "0.3.2"]
+            [lein-eftest "0.5.8"]
+            [lein-codox "0.10.7"]
+            [lein-cljfmt "0.6.4"]
+            [lein-kibit "0.1.6"]
+            [lein-bikeshed "0.5.1"]]
 
-  :profiles {:shared {:dependencies [[org.clojure/clojure "1.10.1"]
-                                     [ring/ring-core "1.8.0"]
-                                     [ring/ring-mock "0.4.0"]
-                                     [nrepl "0.6.0"]
-                                     [eftest "0.5.8"]]}
-             :dev    [:shared {:source-paths ["dev"]
-                               :eftest       {:multithread? false}}]
-             :test   [:shared {:eftest {:multithread? false}}]}
+  :profiles
+  {:shared {:dependencies [[org.clojure/clojure "1.10.1"]
+                           [ring/ring-core "1.8.0"]
+                           [ring/ring-mock "0.4.0"]
+                           [nrepl "0.6.0"]
+                           [eftest "0.5.8"]]}
+   :dev    [:shared {:source-paths ["dev"]
+                     :eftest       {:multithread? false}}]
+   :test   [:shared {:eftest {:multithread? false}}]}
 
-  :aliases {"test" ["with-profile" "test" "eftest" ":all"]})
+  :cloverage
+  {:ns-exclude-regex [#"^user"]}
+
+  :codox
+  {:namespaces  [#"^liberator-hal.ping-resource\."]
+   :metadata    {:doc/format :markdown}
+   :output-path "docs"
+   :doc-paths   ["docs"]
+   :source-uri  "https://github.com/logicblocks/liberator-hal.ping-resource/blob/{version}/{filepath}#L{line}"}
+
+  :cljfmt {:indents ^:replace {#".*" [[:inner 0]]}}
+
+  :deploy-repositories
+  {"releases" {:url "https://repo.clojars.org" :creds :gpg}}
+
+  :release-tasks
+  [["shell" "git" "diff" "--exit-code"]
+   ["change" "version" "leiningen.release/bump-version" "release"]
+   ["codox"]
+   ["changelog" "release"]
+   ["shell" "sed" "-E" "-i" "" "s/\"[0-9]+\\.[0-9]+\\.[0-9]+\"/\"${:version}\"/g" "README.md"]
+   ["shell" "git" "add" "."]
+   ["vcs" "commit"]
+   ["vcs" "tag"]
+   ["deploy"]
+   ["change" "version" "leiningen.release/bump-version"]
+   ["vcs" "commit"]
+   ["vcs" "tag"]
+   ["vcs" "push"]]
+
+  :aliases {"test"      ["with-profile" "test" "eftest" ":all"]
+            "precommit" ["do"
+                         ["check"]
+                         ["kibit"]
+                         ["cljfmt" "fix"]
+                         ["bikeshed"
+                          "--name-collisions" "false"
+                          "--long-lines" "false"
+                          "--verbose" "true"]
+                         ["test"]]})
