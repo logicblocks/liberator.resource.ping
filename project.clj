8fdec54ed834a6cb1d8e24b1e9c8f49200219834
Toby Clemson <tobyclemson@gmail.com>
2020-03-22 18:38:51 +0000

    Pre-release version 0.1.1-RC2 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/liberator-hal.ping-resource "0.1.1-RC1"
+(defproject io.logicblocks/liberator-hal.ping-resource "0.1.1-RC2"
   :description "A HAL ping resource for liberator"
   :url "https://github.com/logicblocks/liberator-hal.ping-resource"
 
