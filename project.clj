121f8674f6bf672efc9a9bd31243a61244348187
Circle CI <circleci@infrablocks.io>
2020-03-29 23:27:11 +0000

    Pre-release version 0.1.3-RC1 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/liberator-hal.ping-resource "0.1.2"
+(defproject io.logicblocks/liberator-hal.ping-resource "0.1.3-RC1"
   :description "A HAL ping resource for liberator"
   :url "https://github.com/logicblocks/liberator-hal.ping-resource"
 
