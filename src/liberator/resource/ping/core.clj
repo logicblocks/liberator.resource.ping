804198d55bbc71abd72c46984c03eb27aee51e09
Toby Clemson <tobyclemson@gmail.com>
2024-05-10 23:19:55 +0100

    Use latest liberator.mixin.

--- src/liberator/resource/ping/core.clj
+++ src/liberator/resource/ping/core.clj
@@ -2,29 +2,33 @@
   (:require
    [halboy.resource :as hal]
    [hype.core :as hype]
-   [liberator.mixin.core :as mixin]
+   [liberator.mixin.core :as lm-core]
+   [liberator.mixin.util :as lm-util]
    [liberator.mixin.json.core :as json-mixin]
    [liberator.mixin.hypermedia.core :as hypermedia-mixin]
    [liberator.mixin.hal.core :as hal-mixin]))
 
 (defn definitions
-  ([{:keys [router]}]
+  ([_]
    {:body (fn [_] {:message "pong"})
 
+    :self-link
+    (fn [{:keys [request router]}]
+      (hype/absolute-url-for request router :ping))
+
     :handle-ok
-    (fn [{:keys [request resource] :as context}]
-      (let [body-fn (:body resource)
-            body (body-fn context)]
+    (fn [context]
+      (let [body (lm-util/resource-attribute-as-value context :body)
+            self-link (lm-util/resource-attribute-as-value context :self-link)]
         (hal/add-properties
-          (hal/new-resource
-            (hype/absolute-url-for request router :ping))
+          (hal/new-resource self-link)
           body)))}))
 
 (defn handler
   ([dependencies]
    (handler dependencies {}))
   ([dependencies overrides]
-   (mixin/build-resource
+   (lm-core/build-resource
      (json-mixin/with-json-mixin dependencies)
      (hypermedia-mixin/with-hypermedia-mixin dependencies)
      (hal-mixin/with-hal-mixin dependencies)
