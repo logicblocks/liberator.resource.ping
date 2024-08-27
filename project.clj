a9dbcd9723a49816e253a5288351daa20ceb9e36
Circle CI <circleci@logicblocks.io>
2024-08-27 12:16:01 +0000

    Pre-release version 0.1.3-RC10 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/liberator.resource.ping "0.1.3-RC9"
+(defproject io.logicblocks/liberator.resource.ping "0.1.3-RC10"
   :description "A ping resource for liberator."
   :url "https://github.com/logicblocks/liberator.resource.ping"
 
